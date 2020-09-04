adcc099ed7ce73ec3dec451f0676e7e5238b79be
Carlos <carlos.e.santaniello@gmail.com>
2020-09-04 21:17:41 +0200

    2.1-variables names

--- js/app.js
+++ js/app.js
@@ -1,10 +1,3 @@
 "use strict" //El modo estricto siempre debe estar en la línea 1
 
-//alert('Hola desde un archivo externo');
-
-//Este es un mensaje de la consola
-console.log('Hola, consola');
-
-var x = "3.1416";
-
-var publicData = "Hola";
+var nombre = "Carlos";
