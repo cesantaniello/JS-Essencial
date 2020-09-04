c7775b892d0632d8dd8470ef5a3bfca864893c86
Carlos <carlos.e.santaniello@gmail.com>
2020-09-04 20:24:48 +0200

    4.Using strict mode

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
+"use strict" //El modo estricto siempre debe estar en la línea 1
+
 //alert('Hola desde un archivo externo');
 
 //Este es un mensaje de la consola
 console.log('Hola, consola');
+
+var x = "3.1416";
+
+var publicData = "Hola";
