58fcffa2909f338585bde07c8a35a6ffb5094e8a
Carlos <carlos.e.santaniello@gmail.com>
2020-09-04 21:25:16 +0200

    2.2-var and let differences

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 "use strict" //El modo estricto siempre debe estar en la línea 1
 
-var nombre = "Carlos";
+var nombre = "Carlos"; //var es usada como variable global
+
+console.log(nombre);
+
+function saludo(){
+    let nombre = "Eduardo"; //let es usado dentro del scope de la función
+    console.log(nombre);
+
+    let edad = 37;
+    console.log(edad);
+};
+
+console.log(edad);
+
+saludo();
