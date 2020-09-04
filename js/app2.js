31086f63beee5c4713963f1c3eae48872f3a17d2
Carlos <carlos.e.santaniello@gmail.com>
2020-09-04 23:17:34 +0200

    4.1 arithmetics operators

---
+++ js/app2.js
@@ -0,0 +1,34 @@
+//Operadores artiméticos
+
+var datoA = 10;
+var datoB = 20;
+
+//SUMA
+var suma = datoA + datoB;
+console.log('La suma de '+datoA+' + '+datoB+' es igual a: ', suma);
+
+//RESTA
+var resta = datoA - datoB;
+console.log('La resta de '+datoA+' - '+datoB+' es igual a: ', resta);
+
+//MULTIPLICACION
+var multiplicacion = datoA * datoB;
+console.log('La multiplicacion de '+datoA+' * '+datoB+' es igual a: ', multiplicacion);
+
+//DIVISION
+var division = datoA / datoB;
+console.log('La division de '+datoA+' / '+datoB+' es igual a: ', division);
+
+//Modulo
+var modulo = datoA % datoB;
+console.log('El modulo de '+datoA+' % '+datoB+' es igual a: ', modulo);
+
+//INCREMENTO
+var incremento = datoA;
+incremento++;
+console.log('El incremento de '+datoA+' es igual a: ', incremento);
+
+//INCREMENTO
+var decremento = datoA;
+decremento--;
+console.log('El decremento de '+datoA+' es igual a: ', decremento);
