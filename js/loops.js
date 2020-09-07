6ba9b6a47717f50b8f16bc291f0c6becb9ffb9ba
Carlos <carlos.e.santaniello@gmail.com>
2020-09-07 18:27:17 +0200

    6.4 break and control flow

--- js/loops.js
+++ js/loops.js
@@ -20,9 +20,27 @@ while(productos > 0){
 */
 
 //Do-While
+/*
 do{
     console.log('Producto vendido');
     productos--;
     debugger;
 }
 while(productos >= 1)
+*/
+
+var contador = 0;
+var cuenta = 0;
+
+for(contador = 0; contador <= 20; contador++){
+    if(contador == 5){
+        break
+    }
+    if(contador % 2 == 0){
+        continue;
+    }    
+    cuenta++;
+    debugger;
+}
+
+console.log("Hay"+cuenta+" números impares");
