5fcb2e33cc4e48fc0fa6a426455860d31460ec4f
Carlos <carlos.e.santaniello@gmail.com>
2020-09-07 16:48:42 +0200

    4.8 typeof

---
+++ js/typeof.js
@@ -0,0 +1,24 @@
+"use strict"
+
+//Operador de tipo de datos
+
+var datoA = 10; //Number
+
+var nombre = "Carlos"; //String
+
+var activo = true; //Boolean
+
+var persona = {
+    edad: 37, //Number
+    deporte: 'Natación' //String
+}//Objeto
+
+console.log(typeof datoA);
+console.log(typeof nombre);
+console.log(typeof activo);
+
+console.log(typeof persona);
+
+console.log(typeof persona.edad);
+console.log(typeof persona.deporte);
+
