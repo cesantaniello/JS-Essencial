c25ca2d077ecada21c1232dda6b244f72eb78c97
Carlos <carlos.e.santaniello@gmail.com>
2020-09-07 17:26:56 +0200

    5.4 nested conditional

--- js/conditionals.js
+++ js/conditionals.js
@@ -1,18 +1,27 @@
 "use strict"
 //Condicionales
 
-//If-Else-If
+//Anidado
 
-var datoA = 10;
+var datoA = 110;
 var datoB = 20;
+var datoC = 5;
 var resultado = "Sin datos";
 
 if (datoA > datoB){
-    resultado = "La primera evaluación se cumplió";
-}else if (datoA == datoB){
-    resultado = "La segunda evaluación se cumplió";
-}else{
-    resultado = "No se cumplió ninguna evaluación";
-}
+    resultado = "La condición se cumplió";
 
-console.log("El resultado de la evaluación if-else-if es: ", resultado);
+    if (datoC < datoB){
+        if (datoC < datoB){
+        resultado = "Evaluación anidada verdadera";
+        }else{
+            resultado = "No se cumplió la evaluación anidada";
+        }
+    }else{
+        resultado = "No se cumplió la evaluación anidada";
+    }
+}else{
+    resultado = "No se cumplió la evaluación";
+}    
+
+console.log("El resultado de la evaluación anidada es: ", resultado);
