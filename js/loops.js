f8307cfaa590c01b0140049b9165274af6b09d43
Carlos <carlos.e.santaniello@gmail.com>
2020-09-07 18:03:59 +0200

    6.2 while loop

--- js/loops.js
+++ js/loops.js
@@ -3,8 +3,17 @@
 
 //For
 var productos = 5;
-
+/*
 for (let contador = 0; contador < productos; contador++){
     console.log("Producto #"+ contador);
+    debugger;
+}
+*/
+
+//While
+
+while(productos > 0){
+    console.log('Producto vendido');
+    productos--;
     debugger;
 }
