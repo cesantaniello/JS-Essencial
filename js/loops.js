1758a874e2bcecb6bda14f7129dd0d23958a19ec
Carlos <carlos.e.santaniello@gmail.com>
2020-09-07 18:10:57 +0200

    6.3 do-while loop

--- js/loops.js
+++ js/loops.js
@@ -11,9 +11,18 @@ for (let contador = 0; contador < productos; contador++){
 */
 
 //While
-
+/*
 while(productos > 0){
     console.log('Producto vendido');
     productos--;
     debugger;
 }
+*/
+
+//Do-While
+do{
+    console.log('Producto vendido');
+    productos--;
+    debugger;
+}
+while(productos >= 1)
