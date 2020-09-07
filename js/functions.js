4a4e377986ecb05d27bf86d242f1742a9a1014fb
Carlos <carlos.e.santaniello@gmail.com>
2020-09-07 23:08:58 +0200

    7.5 SPREAD parameters

--- js/functions.js
+++ js/functions.js
@@ -23,9 +23,37 @@ contar(100);
 */
 
 //Parámetros REST
-
+/*
 function cocinar(...masIngredientes){
     console.log("masIngredientes", masIngredientes);
 }
 
 cocinar("Pollo","Tomate","Arroz","Frijoles","Pescado","Chile");
+*/
+
+//Parámetros SPREAD
+/*
+function cocinar(ingrediente1, ingrediente2, ingrediente3){
+    console.log('Ingrediente 1: ', ingrediente1);
+    console.log('Ingrediente 2: ', ingrediente2, );
+    console.log('Ingrediente 3: ', ingrediente3, );
+}
+
+var ingredientesBase = ["Pollo","Tomate"];
+
+cocinar(...ingredientesBase, "Arroz");
+*/
+
+
+//Parámetros SPREAD + REST
+
+function cocinar(ingrediente1, ingrediente2, ingrediente3, ...otros){
+    console.log('Ingrediente 1: ', ingrediente1);
+    console.log('Ingrediente 2: ', ingrediente2);
+    console.log('Ingrediente 3: ', ingrediente3);
+    console.log('Otros: ', otros)
+}
+
+var ingredientesBase = ["Pollo","Tomate"];
+
+cocinar(...ingredientesBase, "Arroz", "Pescado", "Chile");
