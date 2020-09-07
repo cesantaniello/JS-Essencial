c47b6062793ba8a35ea58a9b7ecf7d8c20f8e5fa
Carlos <carlos.e.santaniello@gmail.com>
2020-09-07 17:36:42 +0200

    5.5 switch conditional

--- js/conditionals.js
+++ js/conditionals.js
@@ -2,7 +2,7 @@
 //Condicionales
 
 //Anidado
-
+/*
 var datoA = 110;
 var datoB = 20;
 var datoC = 5;
@@ -25,3 +25,25 @@ if (datoA > datoB){
 }    
 
 console.log("El resultado de la evaluación anidada es: ", resultado);
+*/
+//Switch con strings
+
+var producto = "Radio";
+var resultado = "";
+
+switch (producto) {
+    case "TV":
+        resultado = "Se eligió la TV";
+        break;
+    case "Radio":
+        resultado = "Se eligió el Radio";
+        break;
+    case "Teléfono":
+        resultado = "Se eligió el Teléfono";
+        break;
+    default:
+        resultado = "No se eligió ningún producto";
+        break;
+}
+
+console.log("El resultado de la evaluación con texto es: ", resultado);
