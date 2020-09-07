3f1945e52c93adf0cc06d2ea8f32b85e3d1e1c79
Carlos <carlos.e.santaniello@gmail.com>
2020-09-07 16:57:09 +0200

    5.1 if conditional

---
+++ js/conditionals.js
@@ -0,0 +1,14 @@
+"use strict"
+//Condicionales
+
+//If
+
+var datoA = 10;
+var datoB = 20;
+var resultado = "Sin datos";
+
+if (datoA > datoB){
+    resultado = "La condición se cumplió";
+}
+
+console.log("El resultado de la evaluación if es: ", resultado);
