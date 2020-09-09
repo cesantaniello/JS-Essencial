7eba6ee626baea5c510052a6aebb337d77709779
Carlos <carlos.e.santaniello@gmail.com>
2020-09-09 13:35:06 +0200

    10.3 numeric properties of instances

--- js/numeric-properties.js
+++ js/numeric-properties.js
@@ -9,7 +9,7 @@ console.log("NaN: ", Number.NaN);
 */
 
 //Métodos númericos
-
+/*
 var numero = "10.301";
 
 console.log('Number: ', typeof numero, typeof Number(numero));
@@ -17,3 +17,13 @@ console.log('parseInt: ', parseInt(numero));
 console.log('parseFloat: ', Number.parseFloat(numero));
 console.log('isNaN: ', isNaN(numero));
 console.log('isInteger: ', Number.isInteger(numero));
+*/
+
+//Propiedades numéricas
+
+var numero = 2.5;
+
+console.log("toExponential: ", numero.toExponential(4));
+console.log("toFixed: ", numero.toFixed(2));
+console.log("toPrecision: ", numero.toPrecision(6));
+console.log("toString: ", typeof numero.toString());
