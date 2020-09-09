799a4639bdbaa25bfd5e1bf63c599de2a968bb8f
Carlos <carlos.e.santaniello@gmail.com>
2020-09-09 13:54:12 +0200

    11.2 length method

--- js/strings.js
+++ js/strings.js
@@ -1,5 +1,12 @@
 "use strict"
-
+/*
 var pais = 'Mexico';
 
 var comida = new String("Ceviche");
+*/
+
+//Medir una cadena de texto
+
+var mensaje = "Estoy aprendiendo JavaScript";
+
+console.log("La cadena de texto tiene: "+mensaje.length+" letras");
