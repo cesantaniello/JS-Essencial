c82aac0b5b31e7e4ec72e3f75a07476b989d23a6
Carlos <carlos.e.santaniello@gmail.com>
2020-09-09 13:28:21 +0200

    10.2 numeric methods

--- js/numeric-properties.js
+++ js/numeric-properties.js
@@ -1,7 +1,19 @@
 "use strict"
-
+/*
+//Propiedades numéricas
 console.log("MAX_VALUE: ", Number.MAX_VALUE);
 console.log("MIN_VALUE: ", Number.MIN_VALUE);
 console.log("NEGATIVE_INFINITY: ", Number.NEGATIVE_INFINITY);
 console.log("POSITIVE_INFINITY: ", Number.POSITIVE_INFINITY);
 console.log("NaN: ", Number.NaN);
+*/
+
+//Métodos númericos
+
+var numero = "10.301";
+
+console.log('Number: ', typeof numero, typeof Number(numero));
+console.log('parseInt: ', parseInt(numero));
+console.log('parseFloat: ', Number.parseFloat(numero));
+console.log('isNaN: ', isNaN(numero));
+console.log('isInteger: ', Number.isInteger(numero));
