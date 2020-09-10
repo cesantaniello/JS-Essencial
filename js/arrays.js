62aa839e8d69322f4a5982142275302c2f6b3d05
Carlos <carlos.e.santaniello@gmail.com>
2020-09-10 13:36:57 +0200

    12.4 basic operations

--- js/arrays.js
+++ js/arrays.js
@@ -13,7 +13,20 @@ console.log("El último platillo es: ",ultimoPlatillo);
 */
 
 //Arreglos multidimensionales
-
+/*
 var menu = [platillos, bebidas];
 
 console.log(menu[1][0])
+*/
+
+//Operaciones básicas de un arreglo
+
+console.log('Antes: ', platillos);
+
+//platillos.push("queso"); La función push se utiliza para agregar elementos al final del arreglo
+//platillos.pop();         La función pOP se utiliza para eliminar elementos del final del arreglo
+
+//console.log('Despues: ', platillos);
+
+var mensaje = platillos.join(); //El método join transforma el arreglo en string
+console.log(mensaje);
