659d8d09824f3b511b48d4a5ce9790419c39ea57
Carlos <carlos.e.santaniello@gmail.com>
2020-09-10 14:14:13 +0200

    12.5 making arrays

--- js/arrays.js
+++ js/arrays.js
@@ -1,5 +1,5 @@
-var platillos = ["ceviche", "tacos", "pasta", "tostadas"];
-var bebidas = new Array("Jamaica", "Chicha", "Pozol");
+//var platillos = ["ceviche", "tacos", "pasta", "tostadas"];
+//var bebidas = new Array("Jamaica", "Chicha", "Pozol");
 
 //console.log(Array.isArray(platillos), bebidas)
 
@@ -21,12 +21,34 @@ console.log(menu[1][0])
 
 //Operaciones básicas de un arreglo
 
-console.log('Antes: ', platillos);
+//console.log('Antes: ', platillos);
 
 //platillos.push("queso"); La función push se utiliza para agregar elementos al final del arreglo
 //platillos.pop();         La función pOP se utiliza para eliminar elementos del final del arreglo
 
 //console.log('Despues: ', platillos);
 
-var mensaje = platillos.join(); //El método join transforma el arreglo en string
-console.log(mensaje);
+
+//var mensaje = platillos.join(); El método join transforma el arreglo en string
+//console.log(mensaje);
+
+
+//Generación de arreglos con split() from() y of()
+
+//Método split
+/*
+var mensaje = "ceviche, tacos, pasta";
+var platillos = mensaje.split(', ');
+
+*/
+
+//Método Array.from()
+/*
+var platillosHTML = Array.from(document.querySelectorAll('.platillos p'))
+var platillos = platillosHTML.map( platillo => platillo.textContent)
+*/
+
+//Método Array.of()
+var platillos = Array.of("ceviche", "tacos", "pasta");
+
+console.log(platillos)
