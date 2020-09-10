fc061aeff295632a30687ca599468221d8e626f2
Carlos <carlos.e.santaniello@gmail.com>
2020-09-10 16:21:35 +0200

    13.2 loop forEach arrays

--- js/looping-arrays.js
+++ js/looping-arrays.js
@@ -1,9 +1,16 @@
 "use strict"
 
 //ITERANDO CON ARREGLOS
-//For in
 var platillos = ["ceviche", "tacos", "pasta", "tostadas"];
 
+//For in
+/*
 for (let i in platillos){
     console.log(platillos[i])
 }
+*/
+
+//ForEach
+//platillos.forEach( platillo => console.log(platillo))
+
+platillos.forEach(( platillo, index ) => console.log("Indice "+ index +" es "+ platillo))
