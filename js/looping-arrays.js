e67a0d9362139d33da16976ee74b7d643156bb2e
Carlos <carlos.e.santaniello@gmail.com>
2020-09-10 16:32:36 +0200

    13.3 search in arrays

--- js/looping-arrays.js
+++ js/looping-arrays.js
@@ -13,4 +13,19 @@ for (let i in platillos){
 //ForEach
 //platillos.forEach( platillo => console.log(platillo))
 
-platillos.forEach(( platillo, index ) => console.log("Indice "+ index +" es "+ platillo))
+//platillos.forEach(( platillo, index ) => console.log("Indice "+ index +" es "+ platillo))
+
+
+//Buscando en un arreglo
+
+//var platoElegido = platillos.find(platillo => platillo == "pasta");
+
+var menu = [
+    {nombre: 'Ceviche', precio: 20, pais: 'Perú'},
+    {nombre: 'Tacos', precio: 10, pais: 'México'},
+    {nombre: 'Pasta', precio: 50, pais: 'Italia'}
+];
+
+var platoElegido = menu.find(platillo => platillo.nombre == "Pasta");
+
+console.log(platoElegido);
