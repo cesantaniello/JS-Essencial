73957300d8ed5717cc633b4f4be8cbadbb2c64b4
Carlos <carlos.e.santaniello@gmail.com>
2020-09-10 13:17:15 +0200

    12.2 length arrays

--- js/arrays.js
+++ js/arrays.js
@@ -1,4 +1,12 @@
-var platillos = ["ceviche", "taacos", "pasta"];
+var platillos = ["ceviche", "tacos", "pasta", "tostadas"];
 var bebidas = new Array("Jamaica", "Chicha", "Pozol");
 
-console.log(Array.isArray(platillos), bebidas)
+//console.log(Array.isArray(platillos), bebidas)
+
+//Medir y acceder a un arreglo
+
+console.log("Hay "+platillos.length+" platillos en el menú");
+
+var ultimoPlatillo = platillos[platillos.length-1];
+
+console.log("El último platillo es: ",ultimoPlatillo);
