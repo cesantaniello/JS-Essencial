a754f4739cc098ddc4ddc3685b74f8f3687640e0
Carlos <carlos.e.santaniello@gmail.com>
2020-09-11 23:39:37 +0200

    16.2 Objects: methods and properties

--- js/objects.js
+++ js/objects.js
@@ -1,10 +1,32 @@
 "use strict";
 
 class Pantalla {
-    constructor(){
+    constructor(marca, modelo, pulgadas){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.pulgadas = pulgadas;
+    }
 
+    encendido(){
+        console.log(`La pantalla ${this.marca} se ha encendido`);
+    }
+
+    volumen(){
+        console.log(`El volumen se ha modificado`);
+    }
+
+    info(){
+        console.log(`La pantalla ${this.marca} de modelo ${this.modelo} es de ${this.pulgadas} pulgadas`);
+    }
+
+    set peso(value){
+        this.kgs = value.trim();
+    }
+
+    get peso(){
+        return this.kgs;
     }
 }
 
-const tvSala = new Pantalla();
-const tvHabitacion = new Pantalla();
+const tvSala = new Pantalla('Master', 'Oasis', 55);
+const tvHabitacion = new Pantalla('Origin', 'Artemis', 80);
