5edbd8beddc559fbbd78359e40d15e9e5fa57f4e
Carlos <carlos.e.santaniello@gmail.com>
2020-09-13 12:48:38 +0200

    17.1 try-catch

---
+++ js/errors.js
@@ -0,0 +1,9 @@
+"use strict";
+
+try{
+    //var array = new Array(100000000000000);
+    //var x = y;
+    decodeURIComponent("http://%ominio.com");
+} catch(error){
+    console.log(error.message)
+}
